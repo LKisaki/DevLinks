6cae7c9d08ebf5fbe6386a14cbd47e6dead9af1b
Luan Kisaki <luankisaki@gmail.com>
2023-07-21 22:10:22 -0300

    update-background-maped

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,12 +1,29 @@
-export default function Background() {
+import { bgObjs } from "./BackgroundObject"
+
+type backgroundProps = {
+  width?: string
+  height?: string
+  vertical?: string
+  horizontal?: string
+  widthLg?: string
+  heightLg?: string
+  verticalLg?: string
+  horizontalLg?: string
+  animationDelay?: string
+  color?: string
+}
+
+export default function Background(props: backgroundProps) {
+  const bgObjects = bgObjs.map(bgObj =>
+    <div
+      key={bgObj.id}
+      className={`absolute rounded-full mix-blend-multiply filter blur-[70px] lg:blur-[110px] animate-blob animation-delay-${bgObj.animationDelay} w-${bgObj.width} h-${bgObj.height} ${bgObj.vertical} ${bgObj.horizontal} lg:w-${bgObj.widthLg} lg:h-${bgObj.heightLg} lg:${bgObj.verticalLg} lg:${bgObj.horizontalLg} bg-${bgObj.color}`}></div>
+  )
   return (
     <div className='select-none -z-10'>
-        <div className='absolute rounded-full mix-blend-multiply filter blur-[90px] animate-blob w-40 h-40 top-56 right-10 lg:w-80 lg:h-80 lg:top-36 lg:right-96 bg-fuchsia-500'></div>
-        <div className='absolute rounded-full mix-blend-multiply filter blur-[90px] animate-blob animation-delay-3 w-40 h-40 top-80 left-14 lg:w-72 lg:h-72 lg:top-0 lg:left-96 bg-yellow-500'></div>
-        <div className='absolute rounded-full mix-blend-multiply filter blur-[90px] animate-blob animation-delay-2 w-40 h-40 top-0 right-80
-        lg:w-72 lg:h-72 lg:top-80 lg:left-96 bg-cyan-500'></div>
-        <div className="flex flex-col items-center justify-center">
-        </div>
-      </div>
+      {bgObjects}
+    </div>
   )
 }
+
+ 
