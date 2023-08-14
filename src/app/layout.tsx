61bbe105d10081dfb0c6c2f35ce416e79e66f7fe
Luan Kisaki <luankisaki@gmail.com>
2023-08-14 16:52:50 -0300

    ThemeSwitcher-position-modificated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Background from '@/components/Background'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${inter.className} dark:bg-gradient-to-b dark:from-gray-950 dark:to-gray-800 dark:text-white max-w-screen min-h-screen`}>
+        <div className='mx-4 my-2'>
+        <ThemeSwitcher />
+        </div>
         {children}
       </body>
     </html>
